a2c7ee23374a94bb71bbb4164e211854d2684125
Manisha Pawar <manishamona200@gmail.com>
2021-01-06 21:21:50 +0530

    first commit

--- 1. Getting Started/10. pythogorean_triplet.java
+++ 1. Getting Started/10. pythogorean_triplet.java
@@ -1,25 +1,28 @@
-//find whether a given triplet is pythagorean or not
-
 import java.util.*;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
+        // write your code here  
         Scanner scn=new Scanner(System.in);
-        int n=scn.nextInt();
+        int a=scn.nextInt();
+        int b=scn.nextInt();
+        int c=scn.nextInt();
         
-        for(int div=2;div * div <= n;div++) {
-            while(n % div == 0) {
-                n = n / div;
-                System.out.print(div+" ");
-            }
+        //assuming a as hypotenuse
+        if(a*a == b*b + c*c) {
+            System.out.print(true);
         }
-        
-        if(n == 1) {
-            //nothing to do
+        //assuming b as hypotenuse
+        else if(b*b == a*a + c*c) {
+            System.out.print(true);
+        }
+        //assuming c as hypotenuse
+        else if(c*c == a*a + b*b) {
+            System.out.print(true);
         }
         else {
-            System.out.println(n);
+            System.out.print(false);
         }
     }
 }
